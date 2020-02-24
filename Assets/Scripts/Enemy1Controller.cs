73299f4caa566d94e4846abafe66419e29172644
onjiro <onjiro.mohyahya@gmail.com>
2020-02-24 22:59:58 +0900

    敵を動かすようにした

--- Assets/Scripts/Enemy1Controller.cs
+++ Assets/Scripts/Enemy1Controller.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy1Controller : MonoBehaviour
 {
+    public PatrolPath path;
     private KinematicStrategy kinematic;
     private Vector2 targetVelocity = Vector2.zero;
 
@@ -11,13 +13,30 @@ public class Enemy1Controller : MonoBehaviour
     void Start()
     {
         this.kinematic = new KinematicStrategy(gameObject);
+
+        this.targetVelocity = Vector2.right * 2;
+        GetComponent<SpriteRenderer>().flipX = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.targetVelocity = Vector2.right * 2;
-        GetComponent<SpriteRenderer>().flipX = false;
+        var rb2d = this.GetComponent<Rigidbody2D>();
+
+        if ((this.targetVelocity.x > 0 && rb2d.position.x >= this.path.EndPositionInGlobal().x)
+            || (this.targetVelocity.x < 0 && rb2d.position.x < this.path.StartPositionInGlobal().x))
+        {
+            this.targetVelocity = Vector2.zero;
+            StartCoroutine(flipAndGoAfter(1f, rb2d));
+        }
+    }
+
+    private IEnumerator flipAndGoAfter(float seconds, Rigidbody2D rb2d)
+    {
+        yield return new WaitForSeconds(seconds);
+        var spriteRenderer = this.GetComponent<SpriteRenderer>();
+        this.targetVelocity = spriteRenderer.flipX ? Vector2.right * 2 : Vector2.left * 2;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
     }
 
     private void FixedUpdate()
---
+++ Assets/Scripts/PatrolPath.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+    public Vector2 startPosition, endPosition;
+    public Vector2 StartPositionInGlobal() => this.transform.TransformPoint(this.startPosition);
+    public Vector2 EndPositionInGlobal() => this.transform.TransformPoint(this.endPosition);
+}
