db4dcd317df5c8b64cdfad229e3a23e871644145
onjiro <onjiro.mohyahya@gmail.com>
2020-02-24 23:40:13 +0900

    ネギ弾着エフェクト追加

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     public float speed;
+    // 着弾エフェクト
+    public GameObject landingEffect;
     public float lifeTimeSeconds;
     private Rigidbody2D rb2d;
     void Start()
@@ -20,5 +22,26 @@ public class BulletController : MonoBehaviour
         var deltaPosition = velocity * Time.deltaTime;
 
         this.rb2d.MovePosition(this.rb2d.position + deltaPosition);
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var other = otherCollider.gameObject;
+        switch (other.tag)
+        {
+            case "Enemy":
+                Instantiate(this.landingEffect, this.rb2d.position + new Vector2(this.speed, 0) * Time.deltaTime, this.transform.rotation);
+                Destroy(gameObject);
+                break;
+            case "Land":
+                Instantiate(this.landingEffect, this.rb2d.position + new Vector2(this.speed, 0) * Time.deltaTime, this.transform.rotation);
+                Destroy(gameObject);
+                break;
+        }
     }
 }
